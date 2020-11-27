32af736a3dc0c2f3ee55207c8262903b7a8a7a0b
rmadar <romain.madar@gmail.com>
2020-11-27 14:06:57 +0100

    First running example

--- demo/main.go
+++ demo/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-
+	"math"
+	
 	"go-hep.org/x/hep/fmom"
 	"go-hep.org/x/hep/groot"
 	"go-hep.org/x/hep/groot/rtree"
 
 	"github.com/rmadar/go-topquark-reco/sonn"
+	"github.com/rmadar/go-lorentz-vector/lv"
 )
 
 func main() {
@@ -34,6 +36,7 @@ func main() {
 		lepPhi []float32
 		lepPid []int32
 		jetPt  []float32
+		jetE   []float32
 		jetEta []float32
 		jetPhi []float32
 		nBjets int32
@@ -46,6 +49,7 @@ func main() {
 			{Name: "d_lep_phi", Value: &lepPhi},
 			{Name: "d_lep_pid", Value: &lepPid},
 			{Name: "d_jet_pt", Value: &jetPt},
+			{Name: "d_jet_e", Value: &jetE},
 			{Name: "d_jet_eta", Value: &jetEta},
 			{Name: "d_jet_phi", Value: &jetPhi},
 			{Name: "d_nbjet", Value: &nBjets},
@@ -66,24 +70,55 @@ func main() {
 
 	// Event loop
 	err = r.Read(func(ctx rtree.RCtx) error {
+		
+		// Prepare leptons four vectors
+		var lP, lbarP lv.FourVec
+		var lId, lbarId int
+		for i, id := range lepPid {
+			if id > 0 {
+				lbarId = int(id)
+				lbarP  = lv.NewFourVecPtEtaPhiM(float64(lepPt[i]), float64(lepEta[i]), float64(lepPhi[i]), 0.0)
+			} else {
+				lId = int(id)
+				lP  = lv.NewFourVecPtEtaPhiM(float64(lepPt[i]), float64(lepEta[i]), float64(lepPhi[i]), 0.0)
+			}
+		}
+		
+		// Prepare jet four vectors based on the 2 leading jets
+		var j1P, j2P lv.FourVec
+		j1P = lv.NewFourVecPtEtaPhiE(float64(jetPt[0]), float64(jetEta[0]), float64(jetPhi[1]), float64(jetE[0]))
+		j2P = lv.NewFourVecPtEtaPhiE(float64(jetPt[1]), float64(jetEta[1]), float64(jetPhi[1]), float64(jetE[1]))
+		
+		// Prepare missing transverse energy component
+		Etx := float64(metMet) * math.Cos(float64(metPhi))
+		Ety := float64(metMet) * math.Sin(float64(metPhi))
+		
+		// Call the Sonnenschein reconstruction
+		tops := sonn.RecoTops(
+			fmomP4from(lP), fmomP4from(lbarP), lId, lbarId,
+			fmomP4from(j1P), fmomP4from(j2P), Etx, Ety, int(nBjets),
+		)
+		
 
 		// Print some information
-		fmt.Printf(
-			"evt[%d]: %v, %v, %v %d\n",
-			ctx.Entry, lepPt, lepEta, lepPhi, lepPid,
-		)
-
-		// Call the Sonnenschein reconstruction
-		// translate input TTree variable into function argument
-		//   * lP, lbarP, jP, jbarP -> fmom.PxPyPzE()
-		//   * lId, lbId, nBjets    -> int
-		//   * Etx, Ety             -> float32 
-		//tops := sonn.RecoTops(lP, lbarP, lId, lbarId, jP, jbarP, Etx, Ety, nBjets)
+		fmt.Printf("Event %d:\n", ctx.Entry)
+		fmt.Printf("   - N[b-jets]    %v\n"  , nBjets)
+		fmt.Printf("   - P4[top]      %v\n"  , tops[0])
+		fmt.Printf("   - P4[anti-top] %v\n\n", tops[1])
 		
+
+
 		return nil
 	})
+	
 	if err != nil {
 		log.Fatalf("could not process tree: %+v", err)
 	}
 	
+}
+
+// Helper function to get a fmom.P4 from lv.FourVec
+func fmomP4from(fv lv.FourVec) fmom.P4 {
+	res := fmom.NewPxPyPzE(fv.Px(), fv.Py(), fv.Pz(), fv.E())
+	return res.Clone()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rmadar/go-topquark-reco
 
 go 1.16
 
-require go-hep.org/x/hep v0.28.3
+require (
+	github.com/rmadar/go-lorentz-vector v0.0.0-20200408140142-c838293f9a81 // indirect
+	go-hep.org/x/hep v0.28.3
+)
